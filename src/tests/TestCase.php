898790d050643294dd39c3470c94b0ddadfe0b71
Rodrigo Tenorio <rt3norio@gmail.com>
2021-12-08 00:23:38 -0300

    refatorando lógica de criação de dados de teste para a classe pai

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -1,9 +1,51 @@
 <?php
 
+use App\Models\Transaction;
+use App\Models\User;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+
+    protected $user1;
+    protected $user2;
+
+    protected function refreshTestData()
+    {
+        Transaction::getQuery()->delete();
+        User::getQuery()->delete();
+
+
+        $this->user1 = User::create([
+            'name' => 'user1',
+            'email' => 'user1@user1.com',
+            'password' => '123', // password
+            'taxCode' => '0000',
+            'store' => false,
+            'balance' => 0
+        ]);
+
+        $this->user2 =  User::create([
+            'name' => 'user2',
+            'email' => 'user2@user2.com',
+            'password' => '123', // password
+            'taxCode' => '0001',
+            'store' => false,
+            'balance' => 0
+        ]);
+    }
+    protected function deleteAllData()
+    {
+        Transaction::getQuery()->delete();
+        User::getQuery()->delete();
+    }
+
     /**
      * Creates the application.
      *
@@ -11,6 +53,6 @@ abstract class TestCase extends BaseTestCase
      */
     public function createApplication()
     {
-        return require __DIR__.'/../bootstrap/app.php';
+        return require __DIR__ . '/../bootstrap/app.php';
     }
 }
