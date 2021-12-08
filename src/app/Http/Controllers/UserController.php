f62d05e8486ab51227fd88ed70feb70e4fda8d92
Rodrigo Tenorio <rt3norio@gmail.com>
2021-12-08 00:21:42 -0300

    melhorando tratamento de erro na classe de usuario

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -5,19 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
 
 class UserController extends BaseController
 {
     //
     public function create(Request $request)
     {
-        return User::firstOrCreate([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password,
-            'taxCode' => $request->taxCode,
-            'store' => $request->store
-        ]);
+        try {
+            return User::firstOrCreate([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+                'taxCode' => $request->taxCode,
+                'store' => $request->store
+            ]);
+        } catch (Throwable $t) {
+            throw new HttpException(422, 'usuario não pode ser criado');
+        }
     }
 
     public function show(Request $request, $id)
